5bb153abdab8d883a7300d15e428e2c98ec7a488
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2025-03-15 11:08:32 +0900

    [Add] Enum
    editor config 및 enum 추가

---
+++ Assets/01_Scripts/Utilities/Enums.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+
+public class Enums
+{
+    public enum Class
+    {
+        Warrior,
+        Paladin,
+        DeathKnight,
+        Evoker,
+        Shaman,
+        Hunter,
+        DemonHunter,
+        Druid,
+        Rogue,
+        Monk,
+        Priest,
+        Warlock,
+        Mage,
+    }
+
+    public enum Roll
+    {
+        Tank,
+        Healer,
+        DPS,
+    }
+
+    public enum Range
+    {
+        Melee,
+        Ranged,
+    }
+
+    public enum Armor
+    {
+        Cloth,
+        Leather,
+        Mail,
+        Plate,
+    }
+
+    public enum Warrior
+    {
+        Arms,
+        Fury,
+        Protection,
+    }
+
+    public enum Paladin
+    {
+        Holy,
+        Protection,
+        Retribution,
+    }
+
+    public enum DeathKnight
+    {
+        Blood,
+        Frost,
+        Unholy,
+    }
+
+    public enum Evoker
+    {
+        Devastation,
+        Augmentation,
+        Preservation,
+    }
+
+    public enum Shaman
+    {
+        Elemental,
+        Enhancement,
+        Restoration,
+    }
+
+    public enum Hunter
+    {
+        BeastMastery,
+        Marksmanship,
+        Survival,
+    }
+
+    public enum DemonHunter
+    {
+
+    }
+
+    public enum Druid
+    {
+
+    }
+
+    public enum Rogue
+    {
+
+    }
+
+    public enum Monk
+    {
+
+    }
+
+    public enum Priest
+    {
+
+    }
+
+    public enum Warlock
+    {
+
+    }
+
+    public enum Mage
+    {
+
+    }
+}

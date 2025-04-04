7ad3385214f6332d3550f6edbfb542f2142efe3e
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2025-04-04 21:12:52 +0900

    [Feat] 색상
    클래스 색상 정의

---
+++ Assets/01_Scripts/Utilities/ColorDefine.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ColorDefine
+{
+    // 게임에 쓰일 색상 정의
+    public static Color WarriorColor = new Color(198/255f, 155/255f, 107/255f);
+    public static Color PaladinColor = new Color(231/255f, 134/255f, 173/255f);
+    public static Color DeathKnightColor = new Color(177/255f, 31/255f, 56/255f);
+    public static Color EvokerColor = new Color(49/255f, 138/255f, 116/255f);
+    public static Color ShamanColor = new Color(14/255f, 113/255f, 214/255f);
+    public static Color HunterColor = new Color(159/255f, 196/255f, 111/255f);
+    public static Color DemonHunterColor = new Color(161/255f, 54/255f, 201/255f);
+    public static Color DruidColor = new Color(249/255f, 122/255f, 41/255f);
+    public static Color RogueColor = new Color(239/255f, 231/255f, 107/255f);
+    public static Color MonkColor = new Color(40/255f, 239/255f, 146/255f);
+    public static Color PriestColor = new Color(243/255f, 247/255f, 243/255f);
+    public static Color WarlockColor = new Color(132/255f, 138/255f, 230/255f);
+    public static Color MageColor = new Color(65/255f, 190/255f, 222/255f);
+
+}

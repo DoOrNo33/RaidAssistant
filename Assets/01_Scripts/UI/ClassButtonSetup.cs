3de1493ceb6386474bea041743862fc71691f9e8
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2025-04-04 10:55:35 +0900

    [Feat] 버튼 색상
    버튼 색상 와우 헤드 기준 추출

--- Assets/01_Scripts/UI/ClassButtonSetup.cs
+++ Assets/01_Scripts/UI/ClassButtonSetup.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ClassButtonSetup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI classNameText;
+    [SerializeField] private Image buttonColor;
 }
